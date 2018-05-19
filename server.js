2a27913cec785e641ed56ed2b5822619252016af
Ashbin <kunal.mazumdar@brillio.com>
2018-05-19 11:45:55 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const hbs = require('hbs')
+const app = express()
+const fs = require('fs')
+
+app.use(express.static(`${__dirname}/public`))
+app.set('view engine', 'hbs')
+hbs.registerPartials(`${__dirname}/views/partials`)
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+hbs.registerHelper('toUpperCase', (text) => {
+    return text.toUpperCase();
+})
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var logs = `${now} - ${req.method} - ${req.path}`
+    fs.appendFile('server.log', logs + '\n', (err) => {
+        if (err)
+            console.log(err)
+    })
+    next();
+})
+// app.use((req, res, next) => {
+//     res.render('maintanence.hbs', {
+//         title: 'Maintanence'
+//     })
+// })
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        title: 'Home',
+        welcomeMessage: 'Welcome to my website'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        title: 'About Page'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('app is running')
+})
